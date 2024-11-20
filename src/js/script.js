7ee1eaa98876eb26f8af6ef4b22d2bb31c4641d8
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2024-11-20 09:54:30 +0100

    JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,26 @@
 // *** CAROUSEL ***
 // TODO
+
+
+carousel = document.querySelector("carousel__container");
+carousel.scrollRight = 520;
+carousel.scrollBy({ left: +260, behavior: "smooth" });
+carousel.scrollBy({ left: -260, behavior: "smooth" });
+prevButton = scrollBy;
+nextButton = scrollBy;
+
+const carousel = carousel__container;
+const prevButton = carousel__container;
+const nextButton = carousel__container;
+
+prevButton.addEventListener("click", () => {
+    carousel__container;
+  });
+       
+  nextButton.addEventListener("click", () => {
+    carousel__container;
+  });
+}
+
+premierItem = document.querySelector(".carousel__item");
+scrollAmount = premierItem.clientWidth;
